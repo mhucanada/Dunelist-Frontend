3169a100470c15a8893b5d8d8eed48a63b3e7bd5
mhucanada <mhucanada1@gmail.com>
2020-08-22 16:22:50 -0400

    changed onclick

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Task = ({ task, toggleFinished, deleteTask }) => {
-    const label = task.status
+/*     const label = task.status
     ? 'finished' : 'not finished'
-
-
+ */
     return (
         <div>
             {/*<button onClick={toggleFinished}>{label}</button>*/}
-            <input type="checkbox" id="myCheck" onclick="myFunction()"></input>
+            <input type="checkbox" id="myCheck" onClick={toggleFinished}></input>
             {task.content}
             <button onClick={deleteTask}>x</button>
         </div>
